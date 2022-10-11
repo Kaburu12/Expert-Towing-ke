9ecfefb3d548cde07cf19931deef8c93af5238ef
michael kaburu <michael.kaburu@student.moringaschool.com>
2022-10-11 17:32:26 +0300

    create vehicle & request data

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,3 +1,2 @@
 class Request < ApplicationRecord
-    belongs_to :user
 end
--- db/migrate/20221011115259_create_requests.rb
+++ db/migrate/20221011115259_create_requests.rb
@@ -2,7 +2,7 @@ class CreateRequests < ActiveRecord::Migration[7.0]
   def change
     create_table :requests do |t|
       t.string :vehicle
-      t.string :place
+      t.string :location
       t.string :phone
 
       t.timestamps
