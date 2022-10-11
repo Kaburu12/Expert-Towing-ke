243a224099840f267deebd6e3975c8ff838d3c26
michael kaburu <michael.kaburu@student.moringaschool.com>
2022-10-11 14:02:18 +0300

    create user model,routes & migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  post '/signup',to:"users#create"
+  post '/login', to: "sessions#create"
+  delete '/logout', to: "sessions#destroy"
+  get '/me', to: "users#show"
+  get '/users', to: "users#index"
+
 end
