dd88aa0992daaf7281cdae874d2ad2209bda7264
michael kaburu <michael.kaburu@student.moringaschool.com>
2022-10-14 01:27:31 +0300

    create RequestList component with mapped request data

--- client/src/components/RequestList.js
+++ client/src/components/RequestList.js
@@ -1,28 +1,28 @@
-// import RequestCard from "./RequestCard";
+import RequestCard from "./RequestCard";
 
-// function RequestList({requests,onDeleteRequest}) {
+function RequestList({requests,onDeleteRequest}) {
 
-//     return (
-//       <div>
-//         <div className="requestheader-cont">
-//           <div className="request-header">
-//           <h3>Updated Requests</h3>
-//           </div>
-//         </div>
-//          <ul className="card-container">
-//         {requests.map((request) => {
-//           return (
-//             <RequestCard
-//               key={request.id}
-//               request={request}
-//               onDeleteRequest={onDeleteRequest}
-//             />
-//           );
-//         })}
-//       </ul>
-//       </div>
+    return (
+      <div>
+        <div className="requestheader-cont">
+          <div className="request-header">
+          <h3>Updated Requests</h3>
+          </div>
+        </div>
+         <ul className="card-container">
+        {requests.map((request) => {
+          return (
+            <RequestCard
+              key={request.id}
+              request={request}
+              onDeleteRequest={onDeleteRequest}
+            />
+          );
+        })}
+      </ul>
+      </div>
      
-//     );
-//   }
+    );
+  }
  
-// export default RequestList
+export default RequestList
