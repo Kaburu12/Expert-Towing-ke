ff9a4a4de38e21d764ef2a18e7cf23b91dec071c
michael kaburu <michael.kaburu@student.moringaschool.com>
2022-10-27 01:14:38 +0300

    add password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
-    # validates :password,length:{minimum:8}
+    validates :password,length:{minimum:8, message:"please enter a password with at least 8 characters"}
 
     has_many :requests 
 end
