9ecca4a1ec331a083fc70d23b06bdb634f122902
michael kaburu <michael.kaburu@student.moringaschool.com>
2022-10-27 01:18:40 +0300

    add a user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+    validates :user ,presence: true, uniqueness: true
     validates :password,length:{minimum:8, message:"please enter a password with at least 8 characters"}
 
     has_many :requests 
